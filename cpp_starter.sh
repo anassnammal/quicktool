f4d74b6046e99e1e5f7753447ddd3ba39b5ef318
ANS <70458907+anassnammal@users.noreply.github.com>
2024-02-03 20:39:54 +0100

    Update cpp_starter.sh

--- cpp_starter.sh
+++ cpp_starter.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
+# Example:
+#       Enter project name:
+#           Polymorphism
+#       Enter project dirname:
+#           ex00
+#       Do you want to create a git repository? (y/n)
+#           n
+#       Do you want separate directories for headers and sources? (y/n)
+#           n
+#       Enter files to create (separated by spaces):
+#           main Makefile Animal Cat Dog
+#   NOTICE :
+#       - If you want to create a class, the name must start with a capital letter 
+#            - .hpp and .cpp is generated in cannonical form.
+#       - If name starts with a lowercase letter, it will be considered as a normal source file.
+#            - an empty .cpp is generated.
+
 cpp_class_gen()
 {
     echo "#include \"$1.hpp\"" >> $2
     echo "" >> $2
     echo "$1::$1(void)" >> $2
     echo "{" >> $2
-    echo "    // std::cout << \"Default constructor called\" << std::endl;" >> $2
+    echo "    // std::cout << \"$1: Default constructor called\" << std::endl;" >> $2
     echo "    return ;" >> $2
     echo "}" >> $2
     echo "" >> $2
     echo "$1::$1($1 const & src)" >> $2
     echo "{" >> $2
-    echo "    // std::cout << \"Copy constructor called\" << std::endl;" >> $2
+    echo "    // std::cout << \"$1: Copy constructor called\" << std::endl;" >> $2
     echo "    *this = src;" >> $2
     echo "    return ;" >> $2
     echo "}" >> $2
     echo "" >> $2
     echo "$1::~$1(void)" >> $2
     echo "{" >> $2
-    echo "    // std::cout << \"Destructor called\" << std::endl;" >> $2
+    echo "    // std::cout << \"$1: Destructor called\" << std::endl;" >> $2
     echo "    return ;" >> $2
     echo "}" >> $2
     echo "" >> $2
@@ -27,16 +44,16 @@ cpp_class_gen()
     echo "{" >> $2
     echo "    if (this != &src)" >> $2
     echo "    {" >> $2
-    echo "        // std::cout << \"Assignation operator called\" << std::endl;" >> $2
+    echo "        // std::cout << \"$1: Copy assignation operator called\" << std::endl;" >> $2
     echo "    }" >> $2
     echo "    return *this;" >> $2
     echo "}" >> $2
     echo "" >> $2
-    echo "std::ostream & operator<<(std::ostream & o, $1 const & src)" >> $2
-    echo "{" >> $2
-    echo "    // o << \"ostream operator called\";" >> $2
-    echo "    return o;" >> $2
-    echo "}" >> $2
+    echo "// std::ostream & operator<<(std::ostream & o, $1 const & src)" >> $2
+    echo "// {" >> $2
+    echo "//     o << \"ostream operator called\";" >> $2
+    echo "//     return o;" >> $2
+    echo "// }" >> $2
     echo "" >> $2
     echo -e "\033[32m$2 created!\033[0m"
 }
@@ -58,7 +75,7 @@ hpp_class_gen()
     echo "    $1 & operator=($1 const & src);" >> $2
     echo "};" >> $2
     echo "" >> $2
-    echo "std::ostream & operator<<(std::ostream & o, $1 const & src);" >> $2
+    echo "// std::ostream & operator<<(std::ostream & o, $1 const & src);" >> $2
     echo "" >> $2
     echo -e "\033[32m$2 created!\033[0m"
 }
@@ -68,12 +85,13 @@ mkf_gen()
     echo "NAME = $NAME" >> Makefile
     echo "SRCS = $MAINFILE ${CPPFILES[@]}" >> Makefile
     echo "OBJS = \$(patsubst %.cpp, %.o, \$(SRCS))" >> Makefile
+    echo "INCS = ${CPPFILES[@]}" >> Makefile
     echo "CPPFLAGS = -std=c++98 -Wall -Wextra -Werror" >> Makefile
     echo "CC = c++" >> Makefile
     echo "" >> Makefile
     echo "all: \$(NAME)" >> Makefile
     echo "" >> Makefile
-    echo "\$(NAME): \$(OBJS)" >> Makefile
+    echo "\$(NAME): \$(OBJS) \$(INCS)" >> Makefile
     echo "	\$(CC) \$(CPPFLAGS) \$(OBJS) -o \$(NAME)" >> Makefile
     echo "" >> Makefile
     if [[ $SUBDIRS == "y" ]]; then
@@ -100,7 +118,7 @@ mkf_gen()
     echo -e "\033[32mMakefile created!\033[0m"
 }
 
-echo "__Project starter generator__"
+echo "__Cpp project generator__"
 
 echo "Enter project name:"
 read NAME
@@ -117,6 +135,7 @@ read SUBDIRS
 echo "Enter files to create (separated by spaces):"
 read FILES
 
+EMPTYCPPFILES=()
 CPPFILES=()
 HPPFILES=()
 MAKEFILE=0
@@ -142,6 +161,8 @@ for FILE in $FILES; do
     elif [[ $FILE =~ ^[A-Z] ]]; then
         CPPFILES+=("$SRCDIR$FILE.cpp")
         HPPFILES+=("$INCDIR$FILE.hpp")
+    else
+        EMPTYCPPFILES+=("$SRCDIR$FILE.cpp")
     fi
 done
 
@@ -175,6 +196,19 @@ if [[ $MAINFILE != "" ]]; then
     echo "" >> $MAINFILE
     echo -e "\033[32m$MAINFILE created!\033[0m"
 fi
+
+
+for FILE in ${EMPTYCPPFILES[@]}; do
+    touch $FILE
+    echo "" >> $FILE
+    echo "void foo(void)" >> $FILE
+    echo "{" >> $FILE
+    echo "    // std::cout << \"Hello world!\" << std::endl;" >> $FILE
+    echo "    return ;" >> $FILE
+    echo "}" >> $FILE
+    echo "" >> $FILE
+    echo -e "\033[32m$FILE created!\033[0m"
+done
 
 for FILE in ${CPPFILES[@]}; do
     touch $FILE

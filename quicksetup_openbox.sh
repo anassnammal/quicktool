c5a353c07e90e726dd959c42f6e34a69a0fe960c
ANS <70458907+anassnammal@users.noreply.github.com>
2024-02-13 06:37:30 +0100

    Update quicksetup_openbox.sh

--- quicksetup_openbox.sh
+++ quicksetup_openbox.sh
@@ -15,43 +15,43 @@ sudo apt install obconf
 sudo apt install obmenu
 
 # Install obmenu-generator
-sudo apt install obmenu-generator
+sudo apt -y install obmenu-generator
 
 # Install obkey
-sudo apt install obkey
+sudo apt -y install obkey
 
 # Install tint2
-sudo apt install tint2
+sudo apt -y install tint2
 
 # Install lxappearance
-sudo apt install lxappearance
+sudo apt -y install lxappearance
 
 # Install nitrogen
-sudo apt install nitrogen
+sudo apt -y install nitrogen
 
 # Install compton
-sudo apt install compton
+sudo apt -y install compton
 
 # Install lxpolkit
-sudo apt install lxpolkit
+sudo apt -y install lxpolkit
 
 # Install lxsession
-sudo apt install lxsession
+sudo apt -y install lxsession
 
 # Install lxsession-edit
-sudo apt install lxsession-edit
+sudo apt -y install lxsession-edit
 
 # Install lxinput
-sudo apt install lxinput
+sudo apt -y install lxinput
 
 # Install lxrandr
-sudo apt install lxrandr
+sudo apt -y install lxrandr
 
 # Install lxhotkey
-sudo apt install lxhotkey
+sudo apt -y install lxhotkey
 
 # Install lxshortcut
-sudo apt install lxshortcut
+sudo apt -y install lxshortcut
 
 # Install lxsession-logout
-sudo apt install lxsession-logout
+sudo apt -y install lxsession-logout

0fa53da73501e2020023b3b09da4322d71b7f25e
ANS <70458907+anassnammal@users.noreply.github.com>
2024-02-13 06:28:52 +0100

    Create quicksetup_openbox.sh

---
+++ quicksetup_openbox.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+# This script is used to setup openbox for the first time on debian 12
+
+# Install xorg
+sudo apt install xorg
+
+# Install openbox
+sudo apt install openbox
+
+# Install obconf
+sudo apt install obconf
+
+# Install obmenu
+sudo apt install obmenu
+
+# Install obmenu-generator
+sudo apt install obmenu-generator
+
+# Install obkey
+sudo apt install obkey
+
+# Install tint2
+sudo apt install tint2
+
+# Install lxappearance
+sudo apt install lxappearance
+
+# Install nitrogen
+sudo apt install nitrogen
+
+# Install compton
+sudo apt install compton
+
+# Install lxpolkit
+sudo apt install lxpolkit
+
+# Install lxsession
+sudo apt install lxsession
+
+# Install lxsession-edit
+sudo apt install lxsession-edit
+
+# Install lxinput
+sudo apt install lxinput
+
+# Install lxrandr
+sudo apt install lxrandr
+
+# Install lxhotkey
+sudo apt install lxhotkey
+
+# Install lxshortcut
+sudo apt install lxshortcut
+
+# Install lxsession-logout
+sudo apt install lxsession-logout
